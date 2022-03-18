c10e76f213c084d45ecdb6694539c518e6ad54af
chasingash <chasingaisling@gmail.com>
2022-03-18 19:38:17 +0000

    Add shuffle function

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,25 @@ const reset = document.querySelector(".reset-btn");
 const playAgain = document.querySelector(".play-again-btn");
 const movesCount = document.querySelector(".moves-counter");
 let moves = 0;
+const timeCounter = document.querySelector(".timer");
+let time;
+let minutes = 0;
+let seconds = 0;
+let timeStart = false;
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+  
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    return array;
+  }
 
 
 
