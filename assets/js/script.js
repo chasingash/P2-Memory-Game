b6ae3b1bb019452b909368c1adb51977b86c864d
chasingash <chasingaisling@gmail.com>
2022-03-18 20:06:34 +0000

    Add reset for all variables and html elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,8 +17,9 @@ let timeStart = false;
 
 // Shuffle function found on http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    let currentIndex = array.length, temporaryValue, randomIndex;
-  
+    let currentIndex = array.length,
+        temporaryValue, randomIndex;
+
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
@@ -27,20 +28,20 @@ function shuffle(array) {
         array[randomIndex] = temporaryValue;
     }
     return array;
-  }
+}
 
-  //Start The Game
-  function startGame() {
-	const shuffledDeck = shuffle(deckCards); 
-	for (let i = 0; i < shuffledDeck.length; i++) {
-		const liTag = document.createElement('LI');
-		liTag.classList.add('card');
-		const addImage = document.createElement("IMG");
-		liTag.appendChild(addImage);
-		addImage.setAttribute("src", "assets/images/" + shuffledDeck[i] + "?raw=true");
-		addImage.setAttribute("alt", "image of vault boy from fallout");
-		deck.appendChild(liTag);
-	}
+//Start The Game
+function startGame() {
+    const shuffledDeck = shuffle(deckCards);
+    for (let i = 0; i < shuffledDeck.length; i++) {
+        const liTag = document.createElement('LI');
+        liTag.classList.add('card');
+        const addImage = document.createElement("IMG");
+        liTag.appendChild(addImage);
+        addImage.setAttribute("src", "assets/images/" + shuffledDeck[i] + "?raw=true");
+        addImage.setAttribute("alt", "image of vault boy from fallout");
+        deck.appendChild(liTag);
+    }
 }
 
 startGame();
@@ -48,23 +49,43 @@ startGame();
 function removeCard() {
 
     while (deck.hasChildNodes()) {
-		deck.removeChild(deck.firstChild);
-	}
+        deck.removeChild(deck.firstChild);
+    }
 }
 
 // Used to update the timer -  https://www.w3schools.com/js/js_timing.asp
 
 function timer() {
-	time = setInterval(function() {
-		seconds++;
-			if (seconds === 60) {
-				minutes++;
-				seconds = 0;
-			}
-		
-		timeCounter.innerHTML = "<i class='fa fa-hourglass-start'></i>" + " Timer: " + minutes + " Mins " + seconds + " Secs" ;
-	}, 1000);
+    time = setInterval(function () {
+        seconds++;
+        if (seconds === 60) {
+            minutes++;
+            seconds = 0;
+        }
+
+        timeCounter.innerHTML = "<i class='fa fa-hourglass-start'></i>" + " Timer: " + minutes + " Mins " + seconds + " Secs";
+    }, 1000);
+}
+/* Used to stop timer once all 8 pairs are matched - https://www.w3schools.com/js/js_timing.asp */
+
+function stopTime() {
+    clearInterval(time);
 }
 
-
-
+// Reset HTML elements and all global variables
+function resetEverything() {
+	stopTime();
+	timeStart = false;
+	seconds = 0;
+	minutes = 0;
+	timeCounter.innerHTML = "<i class='fa fa-hourglass-start'></i>" + " Timer: 00:00";
+	star[1].firstElementChild.classList.add("fa-star");
+	star[2].firstElementChild.classList.add("fa-star");
+	starCount = 3;
+	moves = 0;
+	movesCount.innerHTML = 0;
+	matched = [];
+	opened = [];
+	removeCard();
+	startGame();
+}
