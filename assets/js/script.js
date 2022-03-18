5d3c30b7e5b736b2a9bf9549dbd3e83b92a308c1
chasingash <chasingaisling@gmail.com>
2022-03-18 18:53:20 +0000

    Add array of images for card deck

---
+++ assets/js/script.js
@@ -0,0 +1,2 @@
+// Array of Deck of Card Images
+const deckCards = ["card1.jpg", "card1.jpg", "card2.jpg", "card2.jpg", "card3.jpg", "card3.jpg", "card4.jpg", "card4.jpg", "card5.jpg", "card5.jpg", "card6.jpg", "card6.jpg", "card7.jpg", "card7.jpg", "card8.jpg", "card8.jpg"];
