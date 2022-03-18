abd021f37a324545d64954adc6d246bd51bc84f6
chasingash <chasingaisling@gmail.com>
2022-03-18 19:26:08 +0000

    Create two arrays to store opened and amteched cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,8 @@
-// Array of Deck of Card Images
+// Card Images Array
 const deckCards = ["card1.jpg", "card1.jpg", "card2.jpg", "card2.jpg", "card3.jpg", "card3.jpg", "card4.jpg", "card4.jpg", "card5.jpg", "card5.jpg", "card6.jpg", "card6.jpg", "card7.jpg", "card7.jpg", "card8.jpg", "card8.jpg"];
+// Global Arrays
+
+const deck = document.querySelector(".deck");
+let opened = [];
+let matched = [];
+
